08f3272faa2c177a6ed38adee78195b2336501f9
David Ko <dko289@gmail.com>
2020-06-19 20:05:59 -0400

    Seed data v2

--- server/index.js
+++ server/index.js
@@ -10,30 +10,21 @@ app.listen(PORT, () => {
   console.log('listening on port: ', PORT)
 });
 
-app.get('api/stores', (req, res) => {
-  const queryString = db.Store.find();
-  db.query(queryString, (err, results) => {
-    if (err) {
-      console.log('Rrror retrieving stores from db')
-    } else {
-      console.log('Retrieved stores from db')
-      res.json({
-        stores: results
-      })
-    }
+app.get('/api/stores', (req, res) => {
+  db.Store.find()
+  .then(results => res.json({stores: results}))
+  .catch(err => {
+    console.log("error ")
+    res.json({error: err})
   })
 })
 
-app.get('api/products', (req, res) => {
-  const queryString = db.Product.find();
-  db.query(query, (err, results) => {
-    if (err) {
-      console.log('Error retrieving products from db');
-    } else {
-      console.log('Retrieved products from db')
-      res.json({
-        products: results
-      })
-    }
+app.get('/api/products', (req, res) => {
+  db.Product.find()
+  .then(results => {
+    res.json({stores: results})
+  })
+  .catch(err => {
+    res.json({error: err})
   })
 })
