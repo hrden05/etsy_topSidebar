dfc6396d287433f9a08be9acded5c710d3e78799
David Ko <dko289@gmail.com>
2020-06-22 20:32:41 -0400

    Basic frontend set up

---
+++ client/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      product: '',
+      store: ''
+    }
+  }
+
+  render() {
+    <div>
+      Fakesy
+    </div>
+  }
+}
+
+export default App;
